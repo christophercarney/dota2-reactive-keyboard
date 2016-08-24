2481fdfef66ad5d923b0e19e649e4030831d89c4
crc5464 <crc5464@psu.edu>
2016-08-24 11:31:33 -0400

    Initial commit.

---
+++ corsair_cue_dota/corsair_CUE_dota/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CUE.NET;
+using System.Diagnostics;
+using CUE.NET.Exceptions;
+using CUE.NET.Devices.Keyboard;
+using CUE.NET.Profiles;
+using System.Drawing;
+using CUE.NET.Devices.Keyboard.Enums;
+using System.IO;
+
+namespace corsair_CUE_dota
+{
+    class Program
+    {
+        static void Main()
+        {
+            CorsairKeyboardKeyId[] Health = {CorsairKeyboardKeyId.Escape, CorsairKeyboardKeyId.F1, CorsairKeyboardKeyId.F2, CorsairKeyboardKeyId.F3, CorsairKeyboardKeyId.F4, CorsairKeyboardKeyId.F5,
+                                            CorsairKeyboardKeyId.F6, CorsairKeyboardKeyId.F7 };
+
+            CorsairKeyboardKeyId[] Mana = {CorsairKeyboardKeyId.F8, CorsairKeyboardKeyId.F9, CorsairKeyboardKeyId.F10, CorsairKeyboardKeyId.F11, CorsairKeyboardKeyId.F12, CorsairKeyboardKeyId.PrintScreen,
+                                            CorsairKeyboardKeyId.ScrollLock, CorsairKeyboardKeyId.PauseBreak };
+
+            CueSDK.Initialize();
+            Debug.WriteLine("Initialized with " + CueSDK.LoadedArchitecture + "-SDK");
+
+            CorsairKeyboard keyboard = CueSDK.KeyboardSDK;
+            if (keyboard == null)
+                throw new WrapperException("No keyboard found");
+
+            Process proc = new System.Diagnostics.Process();
+            proc.StartInfo.FileName = "C:\\Python34\\python.exe";
+            proc.StartInfo.Arguments = "-u \"C:\\Users\\Chris\\Documents\\Visual Studio 2015\\Projects\\dota_keyboard_prototype\\dota_keyboard_prototype\\dota_keyboard_prototype.py\"";
+
+
+            proc.StartInfo.RedirectStandardOutput = true;
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.CreateNoWindow = false;
+            proc.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+
+            proc.Start();
+            StreamReader q = proc.StandardOutput;
+            while (!proc.HasExited)
+            {
+                String[] status = q.ReadLine().Split(null);
+                Console.WriteLine(q.ReadLine());
+                if (status[0] == "BG")
+                    continue;
+
+                int health_int = Int32.Parse(status[0]);
+                int mana_int = Int32.Parse(status[1]);
+
+                for (int i = 0; i < 8; i++)
+                {
+                    if (i < health_int)
+                        keyboard[Health[i]].Led.Color = Color.Green;
+                    else
+                        keyboard[Health[i]].Led.Color = Color.Red;
+
+                    if (i < mana_int)
+                        keyboard[Mana[i]].Led.Color = Color.Blue;
+                    else
+                        keyboard[Mana[i]].Led.Color = Color.White;
+                    keyboard.Update();
+                }
+
+            }
+                
+        }
+
+
+    }
+}
---
+++ dota_picture_handler/dota_picture_handler.py
@@ -0,0 +1,68 @@
+﻿from PIL import ImageGrab, Image
+import time
+from functools import reduce
+import signal, os, sys
+
+#returns remaining health as a fraction over 8
+#e.g. 8/8 = full heatlh, 0/8 = dead
+def determineRemainingHealth(data, eigths, x):
+    width = x
+    pixel = x*2 + (eigths * 8) - 3
+    missing_parts = 0
+    while width > 0:
+        if data[pixel][1] <= 160 or data[pixel][1] >= 175:      #acceptable rgb values for green
+            missing_parts += 1
+        else:
+            return 8 - missing_parts
+        width -= eigths
+        pixel -= eigths
+    return 0
+
+def isDotaInForeground(screenGrab):
+    screenGrab.crop((6, 790, 32, 812)).save("box.jpg")  
+    screenGrabCrop = Image.open("box.jpg")
+    pixel = screenGrabCrop.getpixel((13, 11))
+    sum = pixel[0] + pixel[1] + pixel[2]
+    if sum > 165 or sum < 145:
+        return False
+    else:
+        return True
+
+def determineRemainingMana(data, eigths, x):
+    width = x
+    pixel = x*39 + (eigths * 8) - eigths
+    missing_parts = 0
+    while width > 0:
+        if data[pixel][2] <= 60 or data[pixel][2] >= 90:        #RGB values for blue
+            missing_parts += 1
+        else:
+            return 8 - missing_parts
+        width -= eigths
+        pixel -= eigths
+    return 0
+
+def handler(signum, frame):
+    os.remove("data.jpg")
+    os.remove("box.jpg")
+    sys.exit(0)
+
+def main():
+    signal.signal(signal.SIGINT, handler)
+    while(True):
+        time.sleep(0.5)
+        ImageGrab.grab().save("data.jpg", "JPEG")
+        screenGrab = Image.open("data.jpg")
+        if not isDotaInForeground(screenGrab):
+            print("BG")
+            continue
+        screenGrab.crop((675, 912, 1343, 953)).save("data.jpg")        #The box is a 4-tuple defining the left, upper, right, and lower pixel coordinate.
+        screenGrabCrop = Image.open("data.jpg")
+        data = screenGrabCrop.getdata()
+        x = data.size[0]
+        y = data.size[1]
+        eigths = int(x/8)
+
+        print("{0} {1}".format(determineRemainingHealth(data, eigths, x), determineRemainingMana(data, eigths, x)))
+
+if __name__ == '__main__':
+    main()
